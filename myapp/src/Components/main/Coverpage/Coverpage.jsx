e407d0202d7bf4935b3512654e8bafd1720f3d41
richestcoder <saashutosh39@gmail.com>
2022-02-27 12:10:13 +0530

    comitte to main

---
+++ myapp/src/Components/main/Coverpage/Coverpage.jsx
@@ -0,0 +1,41 @@
+import React from 'react'
+
+import {Carousel} from "react-bootstrap"
+
+import landingPic1 from "../../assets/images/all_images/Landing_Page_Photo1.svg"
+import landingPic2 from '../../assets/images/all_images/landingpage2.svg'
+import landingPic3 from '../../assets/images/all_images/landingpage3.svg'
+import landingPic4 from '../../assets/images/all_images/landingpage4.svg'
+import landingPic5 from '../../assets/images/all_images/landingpage5.svg'
+
+
+
+import "../../../node_modules/bootstrap/dist/css/bootstrap.min.css";
+// import "../../../../node_modules/bootstrap/uti";
+export const CoverPage = () => {
+    return (
+      <div className='imageContainer'>
+         
+          <Carousel fade  >
+              <Carousel.Item interval={500}>
+<img className='imageshow' src={landingPic1}  alt="" />
+              </Carousel.Item>
+              <Carousel.Item interval={500}>
+              <img className='imageshow' src={landingPic2} height="100%" alt="" />
+
+              </Carousel.Item>
+              <Carousel.Item interval={500}>
+              <img className='imageshow' src={landingPic3} height="100%" alt="" />
+
+              </Carousel.Item>
+              <Carousel.Item interval={500}>
+              <img className='imageshow' src={landingPic4} height="100%" alt="" />
+
+              </Carousel.Item>
+          </Carousel>
+        
+          
+      </div>
+    )
+}
+
